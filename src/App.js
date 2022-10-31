c23bcf2ecaf43fa6f720327c86399bde959cefe6
anido_ivan <ivaneanido@gmail.com>
2022-10-31 19:45:05 -0300

    fa

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
-import { BrowserRouter,Route,Navigate,Routes }  from "react-router-dom"
+import { BrowserRouter,Route,Routes }  from "react-router-dom"
 import CartProvider from './context/CartContext';
 import Cart from './components/Cart/Cart';
 
